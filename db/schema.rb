176c1e6ed6f09baf098913a1c3a4a56f3f4d6cf1
sureshprasanna70 <sureshprasanna70@gmail.com>
2015-02-06 19:08:41 +0530

    After migrate

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150206131246) do
+ActiveRecord::Schema.define(version: 20150206133630) do
+
+  create_table "employees", force: true do |t|
+    t.string   "name"
+    t.text     "area"
+    t.string   "proficiency"
+    t.boolean  "available"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "projects", force: true do |t|
     t.string   "title"
