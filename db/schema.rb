01ab66c670cc44996e439657c1d407477eb5da26
sureshprasanna70 <sureshprasanna70@gmail.com>
2015-02-06 18:44:15 +0530

    Added projects scaffold

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150206124001) do
+ActiveRecord::Schema.define(version: 20150206131246) do
+
+  create_table "projects", force: true do |t|
+    t.string   "title"
+    t.text     "desc"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "users", force: true do |t|
     t.string   "email",                  default: "", null: false
